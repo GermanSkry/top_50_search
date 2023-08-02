6da9176c5398217adc6c9d50687f4a2cb9aa5c5b
GermanSkry <114933904+GermanSkry@users.noreply.github.com>
2023-08-02 16:34:37 +0200

    main.py

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import heapq
 import pickle
 import datetime
 
+# Function to read the log file and filter out invalid rows
 def read_log_file(file_path):
     rows = []
     with open(file_path, 'r') as file:
@@ -16,48 +17,54 @@ def read_log_file(file_path):
                 rows.append(row)
     return rows
 
-def calculate_top_50_songs(filtered_rows):
-    top_50_songs_per_country = {}
+# Function to records all songs for each country and save in intermediate files
+def calculate_all_songs_per_country(filtered_rows):
+    songs_per_country = {}
     # Loop through the filtered rows and extract song IDs, user IDs, and the number of streams
     for row in filtered_rows:
         song_id, user_id, country = map(str.strip, row.split('|'))  # Extract song_id, user_id, and country from the row
 
         # Create a dictionary for each country if it doesn't exist
-        top_50_songs_per_country.setdefault(country, {})
+        if country not in songs_per_country:
+            songs_per_country[country] = {}
 
         # Increment the streams count for the current song in the corresponding country
-        top_50_songs_per_country[country][song_id] = top_50_songs_per_country[country].get(song_id, 0) + 1
+        songs_per_country[country][song_id] = songs_per_country[country].get(song_id, 0) + 1
 
-    # Write intermediate results to disk and free up memory
-    for country in top_50_songs_per_country:
-        # Convert the country dictionary to a list of tuples and use a heap to find the top 50 songs
-        top_50_songs = heapq.nlargest(50, top_50_songs_per_country[country].items(), key=lambda x: x[1])
+    # Write intermediate results to disk for each country using pickle serialization
+    for country, songs in songs_per_country.items():
+        with open(f"all_songs_intermediate_{country}.pkl", 'wb') as file:
+            pickle.dump(list(songs.keys()), file)
+
+    return songs_per_country
+
+def calculate_top_50_songs_from_files(all_songs_per_country):
+    top_50_songs_per_country = {}
+    # Calculate the top 50 songs for each country based on the play count
+    for country, songs in all_songs_per_country.items():
+        top_50_songs = heapq.nlargest(50, songs, key=lambda x: songs[x])
         top_50_songs_per_country[country] = top_50_songs
-        # Write the intermediate result to disk for each country using pickle serialization
-        with open(f"top50_intermediate_{country}.pkl", 'wb') as file:
-            pickle.dump(top_50_songs, file)
-
-    # Select the top 50 songs for each country by loading the intermediate results from disk
-    for country in top_50_songs_per_country:
-        with open(f"top50_intermediate_{country}.pkl", 'rb') as file:
-            top_50_songs_per_country[country] = pickle.load(file)
 
     return top_50_songs_per_country
 
+# Function to write the top 50 songs for each country to separate files
 def write_top_50_songs_to_files(top_50_songs_per_country):
     current_date = datetime.date.today().strftime("%Y%m%d")
     for country, top_50_songs in top_50_songs_per_country.items():
         filename = f"country_top50_{country}_{current_date}.txt"
         with open(filename, 'w') as file:
             file.write(f"{country}|")
-            song_data = ','.join([f"{song_id}:{streams}" for song_id, streams in top_50_songs])
+            song_data = ','.join(top_50_songs)
             file.write(song_data)
 
+# Main function to run the entire process
 def main():
-    file_name = 'generated_data.txt'  # Replace YYYYMMDD with the specific date
+    current_date = datetime.date.today().strftime("%Y%m%d")
+    file_name = f'listen-{current_date}.log'
     log_file_path = os.path.join('C:/Users/skrge/', file_name)
     filtered_rows = read_log_file(log_file_path)
-    top_50_songs_per_country = calculate_top_50_songs(filtered_rows)
+    all_songs_per_country = calculate_all_songs_per_country(filtered_rows)
+    top_50_songs_per_country = calculate_top_50_songs_from_files(all_songs_per_country)
     write_top_50_songs_to_files(top_50_songs_per_country)
 
 if __name__ == "__main__":
